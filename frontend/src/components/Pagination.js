d25ba607960f5d86615955a763ae8cc8af57cb57
ericw142 <eric.h.warshawsky@gmail.com>
2024-04-18 12:10:36 -0400

    pagination component styles

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -13,21 +13,76 @@ const Pagination = ({ page, setPage, totalPages}) => {
         setPage(next);
     }
 
-    const goToFirstPage = () => {
-        setPage(1)
+    const goToSecondNextPage = () => {
+        if (page === totalPages) return;
+        const secondNext = page + 2;
+        setPage(secondNext);
     }
 
     const goToLastPage = () => {
         setPage(totalPages)
     }
 
+    const paginationButtonCSS = "font-bold text-gray-700 rounded-full bg-gray-100 flex items-center justify-center h-[40px] w-[40px] mx-1 cursor-pointer hover:bg-gray-400 hover:text-white";
+
     return (
-        <div className='p-4 flex flex-row justify-center'>
-            <button className='pagination-button' onClick={goToFirstPage}><span>&#171;</span></button>
-            <button className='pagination-button' onClick={goToPreviousPage}><span>&#8249;</span></button>
-            <p className='font-bold mt-2 mx-4'>{page} of {totalPages}</p>
-            <button className='pagination-button' onClick={goToNextPage}><span>&#8250;</span></button>
-            <button className='pagination-button' onClick={goToLastPage}><span>&#187;</span></button>
+        <div className='absolute inset-x-0 bottom-0 h-16 sm:ml-64 grow border-t'>
+            <div className='p-4 flex flex-row justify-between'>
+                <button className='text-gray-700' onClick={goToPreviousPage}>Previous</button>
+                <div className='flex flex-row'>
+                    <div 
+                        className={paginationButtonCSS}
+                    >
+                        {page}
+                    </div>
+                    {totalPages >= page + 1 && (
+                        <div
+                            onClick={goToNextPage}
+                            className={paginationButtonCSS}
+                        >
+                            {page + 1}
+                        </div>
+                    )}
+                    {totalPages >= page + 2 && (
+                        <div
+                            onClick={goToSecondNextPage}
+                            className={paginationButtonCSS}
+                        >
+                            {page + 2}
+                        </div>
+                    )}
+                    {totalPages >= page + 3 && (
+                        <div
+                            className="font-bold text-gray-700 flex items-center justify-center h-[40px] w-[40px]" 
+                        >
+                            ...
+                        </div>
+                    )}
+                    {page <= totalPages - 2 && (
+                        <div
+                            className={paginationButtonCSS}
+                        >
+                            {totalPages - 2}
+                        </div>
+                    )}
+                    {page <= totalPages - 1 && (
+                        <div
+                            className={paginationButtonCSS}
+                        >
+                            {totalPages - 1}
+                        </div>
+                    )}
+                    {page < totalPages && (
+                        <div
+                            onClick={goToLastPage}
+                            className={paginationButtonCSS}
+                        >
+                            {totalPages}
+                        </div>
+                    )}
+                </div>
+                <button className='text-gray-700' onClick={goToNextPage}>Next</button>
+            </div>
         </div>
     )
 }
--- frontend/src/components/teacher/TeacherGrid.js
+++ frontend/src/components/teacher/TeacherGrid.js
@@ -22,7 +22,7 @@ const TeacherGrid = ({ teachers }) => {
                     <h1 className='text-start text-2xl font-bold'>Teachers ({teachers?.length})</h1>
                     <h6 className='text-start text-md text-gray-500'>All of the teachers are listed here</h6>
                 </div>
-                <button className='bg-gray-800 text-white px-1 py-0'>+ Add New Teacher</button>
+                <button className='text-gray-700'>+ Add New Teacher</button>
             </div>
             <div className='w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5'>
                 {teachers.map((teacher, i) => {
