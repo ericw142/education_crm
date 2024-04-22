802798319a5bbc6aa77dbb09de3653f9fafdeb10
ericw142 <eric.h.warshawsky@gmail.com>
2024-04-22 13:15:49 -0400

    updated ux for course planner

--- frontend/src/components/course/CoursePlanner.js
+++ frontend/src/components/course/CoursePlanner.js
@@ -4,6 +4,7 @@ import EditCourseForm from './editor/EditCourseForm'
 
 const CoursePlanner = ({ courses, teachers, fetchCourseInfo }) => {
     const [selectedForm, setSelectedForm] = useState('')
+    const [editedCourseData, setEditedCourseData] = useState({})
 
     const backButton = () => {
         return (
@@ -19,32 +20,34 @@ const CoursePlanner = ({ courses, teachers, fetchCourseInfo }) => {
     return (
         <div className='px-4 pt-5'>
             <h1 className='text-center text-2xl font-bold'>Course Planner</h1>
-            {selectedForm === 'New Course' ? (
-                <div>
-                    {backButton()}
-                    <NewCourseForm teachers={teachers} fetchCourseInfo={fetchCourseInfo}/>
-                </div>
-            ) : selectedForm === 'Edit Course' ? (
-                <div>
-                    {backButton()}
-                    <EditCourseForm courses={courses} teachers={teachers} fetchCourseInfo={fetchCourseInfo} />
-                </div>
-            ) : (
-                <div className='container flex flex-col gap-4 p-10 px-[40px]'>
-                    <button
-                        onClick={() => setSelectedForm('Edit Course')}
-                        className="bg-white border border-blue-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1.5 h-[40px] w-[400px] text-[18px] mx-auto"
-                    >
-                        Edit Existing Course
-                    </button>
-                    <button
-                        onClick={() => setSelectedForm('New Course')}
-                        className="bg-white border border-green-300 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-1.5 h-[40px] w-[400px] text-[18px] mx-auto"
-                    >
-                        Create New Course
-                    </button>
-                </div>
-            )}
+            <div>
+                {selectedForm === 'New Course' ? (
+                    <div>
+                        {backButton()}
+                        <NewCourseForm teachers={teachers} fetchCourseInfo={fetchCourseInfo}/>
+                    </div>
+                ) : (
+                    <div>
+                        <div className='flex flex-row justify-between'>
+                            {editedCourseData?._id && (
+                                <button
+                                    onClick={() => setEditedCourseData({})}
+                                    className="bg-white border border-gray-300 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block p-1.5 h-[40px] w-[200px] mb-5"
+                                >
+                                    Back
+                                </button>
+                            )}
+                            <button
+                                onClick={() => setSelectedForm('New Course')}
+                                className="bg-green border border-green-300 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-1.5 h-[40px] w-[200px] mb-5 ml-auto"
+                            >
+                                New Course
+                            </button>
+                        </div>
+                        <EditCourseForm courses={courses} teachers={teachers} fetchCourseInfo={fetchCourseInfo} editedCourseData={editedCourseData} setEditedCourseData={setEditedCourseData} />
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
--- frontend/src/components/course/editor/EditCourseForm.js
+++ frontend/src/components/course/editor/EditCourseForm.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import CourseList from './CourseList'
 
-const EditCourseForm = ({ courses, teachers, fetchCourseInfo }) => {
-    const [editedCourseData, setEditedCourseData] = useState({})
+const EditCourseForm = ({ courses, teachers, fetchCourseInfo, editedCourseData, setEditedCourseData }) => {
     const [resultStatusMessage, setResultStatusMessage] = useState('')
 
     const formatForDateInput = (dateVal) => {
@@ -48,139 +47,141 @@ const EditCourseForm = ({ courses, teachers, fetchCourseInfo }) => {
     }, [resultStatusMessage])
 
     return (
-        <div className='flex flex-row gap-2'>
-            <CourseList courses={courses} onClickFn={setEditedCourseData} selectedId={editedCourseData?._id} />
+        <div>
+            <div className='flex flex-row gap-2'>
+                <CourseList courses={courses} onClickFn={setEditedCourseData} selectedId={editedCourseData?._id} />
 
-            <div className='container'>
-                {resultStatusMessage && <p>{resultStatusMessage}</p>}
-                {editedCourseData?._id && (
-                    <div>
-                        <div className='flex flex-row justify-between pb-5'>
-                            <button
-                                type="button"
-                                onClick={patchCourse}
-                                className="bg-white border border-green-300 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-1.5 h-[40px] text-green-600"
-                            >
-                                Update Course
-                            </button>
-                            <button
-                                type="button"
-                                onClick={deleteCourse}
-                                className="bg-red border border-red-300 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block p-1.5 h-[40px] text-red-500"
-                            >
-                                Delete Course
-                            </button>
-                        </div>
-                        <form>
-                            <div className="mb-5 flex flex-row gap-2">
-                                <div className='container'>
-                                    <label htmlFor='title' className="block mb-2 text-sm font-medium">Title</label>
+                <div className='container'>
+                    {resultStatusMessage && <p>{resultStatusMessage}</p>}
+                    {editedCourseData?._id && (
+                        <div>
+                            <div className='flex flex-row justify-between pb-5'>
+                                <button
+                                    type="button"
+                                    onClick={patchCourse}
+                                    className="bg-white border border-green-300 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-1.5 h-[40px] text-green-600"
+                                >
+                                    Update Course
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={deleteCourse}
+                                    className="bg-red border border-red-300 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block p-1.5 h-[40px] text-red-500"
+                                >
+                                    Delete Course
+                                </button>
+                            </div>
+                            <form>
+                                <div className="mb-5 flex flex-row gap-2">
+                                    <div className='container'>
+                                        <label htmlFor='title' className="block mb-2 text-sm font-medium">Title</label>
+                                        <input
+                                            required
+                                            type="text"
+                                            id='title'
+                                            value={editedCourseData?.title}
+                                            onChange={(e) => {
+                                                setEditedCourseData({ ...editedCourseData, title: e.currentTarget.value });
+                                            }}
+                                            className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        />
+                                    </div>
+                                    <div className='container'>
+                                        <label htmlFor='teacher' className="block mb-2 text-sm font-medium">Teacher</label>
+                                        <select
+                                            value={editedCourseData?.teacherId}
+                                            className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-white"
+                                            onChange={(e) => {
+                                                setEditedCourseData({ ...editedCourseData, teacherId: e.currentTarget.value })
+                                            }}
+                                        >
+                                            <option value=""></option>
+                                            {teachers?.map((teacher, i) => {
+                                                return <option key={teacher?._id} value={teacher?._id}>{teacher?.firstName} {teacher?.lastName}</option>
+                                            })}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="mb-5">
+                                    <label htmlFor='description' className="block mb-2 text-sm font-medium">Description</label>
                                     <input
                                         required
                                         type="text"
-                                        id='title'
-                                        value={editedCourseData?.title}
+                                        id='description'
+                                        value={editedCourseData?.description}
                                         onChange={(e) => {
-                                            setEditedCourseData({ ...editedCourseData, title: e.currentTarget.value });
+                                            setEditedCourseData({ ...editedCourseData, description: e.currentTarget.value });
                                         }}
                                         className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                     />
                                 </div>
-                                <div className='container'>
-                                    <label htmlFor='teacher' className="block mb-2 text-sm font-medium">Teacher</label>
-                                    <select
-                                        value={editedCourseData?.teacherId}
-                                        className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-white"
-                                        onChange={(e) => {
-                                            setEditedCourseData({ ...editedCourseData, teacherId: e.currentTarget.value })
-                                        }}
-                                    >
-                                        <option value=""></option>
-                                        {teachers?.map((teacher, i) => {
-                                            return <option key={teacher?._id} value={teacher?._id}>{teacher?.firstName} {teacher?.lastName}</option>
-                                        })}
-                                    </select>
+                                <div className="mb-5 flex flex-row gap-2">
+                                    <div className='container'>
+                                        <label htmlFor='startDate' className="block mb-2 text-sm font-medium">Start Date</label>
+                                        <input
+                                            required
+                                            type="date"
+                                            id='startDate'
+                                            value={editedCourseData?.startDate ? formatForDateInput(new Date(editedCourseData.startDate)) : ''}
+                                            onChange={(e) => {
+                                                setEditedCourseData({ ...editedCourseData, startDate: e.currentTarget.value });
+                                            }}
+                                            className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        />
+                                    </div>
+                                    <div className='container'>
+                                        <label htmlFor='endDate' className="block mb-2 text-sm font-medium">End Date</label>
+                                        <input
+                                            required
+                                            type="date"
+                                            id='endDate'
+                                            value={editedCourseData?.endDate ? formatForDateInput(new Date(editedCourseData.endDate)) : ''}
+                                            onChange={(e) => {
+                                                setEditedCourseData({ ...editedCourseData, endDate: e.currentTarget.value });
+                                            }}
+                                            className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="mb-5">
-                                <label htmlFor='description' className="block mb-2 text-sm font-medium">Description</label>
-                                <input
-                                    required
-                                    type="text"
-                                    id='description'
-                                    value={editedCourseData?.description}
-                                    onChange={(e) => {
-                                        setEditedCourseData({ ...editedCourseData, description: e.currentTarget.value });
-                                    }}
-                                    className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                />
-                            </div>
-                            <div className="mb-5 flex flex-row gap-2">
-                                <div className='container'>
-                                    <label htmlFor='startDate' className="block mb-2 text-sm font-medium">Start Date</label>
-                                    <input
-                                        required
-                                        type="date"
-                                        id='startDate'
-                                        value={editedCourseData?.startDate ? formatForDateInput(new Date(editedCourseData.startDate)) : ''}
-                                        onChange={(e) => {
-                                            setEditedCourseData({ ...editedCourseData, startDate: e.currentTarget.value });
-                                        }}
-                                        className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    />
+                                <div className='mb-5 flex flex-row gap-2'>
+                                    <div className='container'>
+                                        <label htmlFor='certificationLevel' className='block mb-2 text-sm font-medium'>JLPT Level</label>
+                                        <select
+                                            value={editedCourseData?.certificationLevel}
+                                            className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-white"
+                                            onChange={(e) => {
+                                                setEditedCourseData({ ...editedCourseData, certificationLevel: e.currentTarget.value })
+                                            }}
+                                        >
+                                            <option value="N5">N5</option>
+                                            <option value="N4">N4</option>
+                                            <option value="N3">N3</option>
+                                            <option value="N2">N2</option>
+                                            <option value="N1">N1</option>
+                                        </select>
+                                    </div>
+                                    <div className='container'>
+                                        <label htmlFor='areaOfFocus' className='block mb-2 text-sm font-medium'>Area of Focus</label>
+                                        <select
+                                            value={editedCourseData?.areaOfFocus}
+                                            className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-white"
+                                            onChange={(e) => {
+                                                setEditedCourseData({ ...editedCourseData, areaOfFocus: e.currentTarget.value })
+                                            }}
+                                        >
+                                            <option value="Vocabulary">Vocabulary</option>
+                                            <option value="Grammar">Grammar</option>
+                                            <option value="Listening">Listening</option>
+                                            <option value="Speaking">Speaking</option>
+                                            <option value="Reading">Reading</option>
+                                            <option value="Writing">Writing</option>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div className='container'>
-                                    <label htmlFor='endDate' className="block mb-2 text-sm font-medium">End Date</label>
-                                    <input
-                                        required
-                                        type="date"
-                                        id='endDate'
-                                        value={editedCourseData?.endDate ? formatForDateInput(new Date(editedCourseData.endDate)) : ''}
-                                        onChange={(e) => {
-                                            setEditedCourseData({ ...editedCourseData, endDate: e.currentTarget.value });
-                                        }}
-                                        className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    />
-                                </div>
-                            </div>
-                            <div className='mb-5 flex flex-row gap-2'>
-                                <div className='container'>
-                                    <label htmlFor='certificationLevel' className='block mb-2 text-sm font-medium'>JLPT Level</label>
-                                    <select
-                                        value={editedCourseData?.certificationLevel}
-                                        className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-white"
-                                        onChange={(e) => {
-                                            setEditedCourseData({ ...editedCourseData, certificationLevel: e.currentTarget.value })
-                                        }}
-                                    >
-                                        <option value="N5">N5</option>
-                                        <option value="N4">N4</option>
-                                        <option value="N3">N3</option>
-                                        <option value="N2">N2</option>
-                                        <option value="N1">N1</option>
-                                    </select>
-                                </div>
-                                <div className='container'>
-                                    <label htmlFor='areaOfFocus' className='block mb-2 text-sm font-medium'>Area of Focus</label>
-                                    <select
-                                        value={editedCourseData?.areaOfFocus}
-                                        className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-white"
-                                        onChange={(e) => {
-                                            setEditedCourseData({ ...editedCourseData, areaOfFocus: e.currentTarget.value })
-                                        }}
-                                    >
-                                        <option value="Vocabulary">Vocabulary</option>
-                                        <option value="Grammar">Grammar</option>
-                                        <option value="Listening">Listening</option>
-                                        <option value="Speaking">Speaking</option>
-                                        <option value="Reading">Reading</option>
-                                        <option value="Writing">Writing</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                )}
+                            </form>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
